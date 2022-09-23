b5f9c18fe0e259f710703102d42e3025b3e1fafd
Akashbala998 <Balaakash998@gmail.com>
2022-09-23 13:53:07 +0530

    time

---
+++ Datas.java
@@ -0,0 +1,14 @@
+package codes;
+
+public class Datas {	
+		
+	
+private void manager() {
+	System.out.println("the manager");
+}
+
+public static void main(String[] args) {
+	Datas d=new Datas();
+	d.manager();
+}
+}
